a09f1d18265aa380b342a85d5ffc09fcaf2de454
VenkatgiriS <venkatgiri877@gmail.com>
2022-05-09 23:07:54 +0530

    commited without keys

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
   access_key = "####"
-<<<<<<< HEAD
   secret_key = "###"
-=======
-  secret_key = "####"
->>>>>>> parent of 1446cc8 (jenkins installation)
 }
 
 module "opsmx" {
